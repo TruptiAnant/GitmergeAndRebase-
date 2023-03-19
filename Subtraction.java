a7b6d61628ac1502143cd2a778313600376452cd
TruptiAnant <127496325+TruptiAnant@users.noreply.github.com>
2023-03-19 19:02:53 +0000

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        System.out.println("This is my subtraction program");
+
+        int a = 8, b = 7;
+        int c = a - b;
+        System.out.println("result=  "+c);
+    }
+}
---
+++ src/Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        System.out.println("This program for addition");
+        int a=9;
+        int b=8;
+        int c=a+b;
+        System.out.println("Result ="+c);
+    }
+}
